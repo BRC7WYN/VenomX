b613584b34a939ec9643f94198f869baca2961f7
BRC7WYN <ashalanpikachu81@GMAIL.COM>
2024-12-13 16:44:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/xkrishmishra/VenomX",
+    "https://github.com/BRC7WYN/VenomX",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
